ec3c0009943eb3c945b4100bc530b82aea75188d
mahimahamsa <160595858+mahimahamsa@users.noreply.github.com>
2024-03-24 10:21:33 +0530

    Add files via upload

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 8: Making scoreboard functional
+let score = localStorage.getItem("score");
+console.log(score);
+
+var scoreBoard = document.getElementById("score-board");
+
+var playAgainButton = document.getElementById("play-again-button");
+
+scoreBoard.innerHTML = score;
+playAgainButton.onclick = (evt) => {
+  location.href = "./game.html";
+};
